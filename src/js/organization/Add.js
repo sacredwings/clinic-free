b37015f85941c7c69808116077ea31ba6accfb92
stronggeniusa@gmail.com <genius36329>
2021-06-07 16:11:06 +0700

    Форма организации

--- src/js/organization/Add.js
+++ src/js/organization/Add.js
@@ -4,10 +4,24 @@ import axios from "axios";
 
 function ArticleAdd (props) {
     let [form, setForm] = useState({
-        inputTitle: '',
-        inputText: '',
-        add: false,
-        err: false
+        name: '',
+        fullName: '',
+        inn: '',
+        kpp: '',
+        ogrn: '',
+        paymentAccount: '', //расчетный счет
+
+        postCode: '', //почтовый индекс
+        country: '', //страна
+        region: '', //область
+        district: '', //район
+        locality: '', //населенный пункт
+        street: '', //улица
+        house: '', //дом
+        corps: '', //корпус
+        structure: '', //строение
+        flat: '', //квартира
+
     })
     let [fileIds, setFileIds] = useState('')
 
@@ -67,23 +81,101 @@ function ArticleAdd (props) {
     }
 
     const Form = () => {
-        return <form onSubmit={onFormSubmit}>
+        return <div className="card m-3">
+            <div className="card-body">
+                <form onSubmit={onFormSubmit} className="p-3">
 
-            <div className="mb-3">
-                <label htmlFor="inputTitle" className="form-label">Название</label>
-                <input type="text" className="form-control" id="inputTitle" onChange={onChangeText} value={form.inputTitle}/>
+                    <h4>Наименование</h4>
+                    <div className="mb-3 row">
+                        <label htmlFor="name" className="col-sm-2 col-form-label">Наименование</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="name" value={form.name} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="fullName" className="col-sm-2 col-form-label">Полное наименование</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="name" value={form.fullName} onChange={onChangeText}/></div>
+                    </div>
+
+                    <hr/>
+                    <h4>Реквизиты</h4>
+                    <div className="mb-3 row">
+                        <label htmlFor="inn" className="col-sm-2 col-form-label">ИНН</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="inn" value={form.inn} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="kpp" className="col-sm-2 col-form-label">КПП</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="kpp" value={form.kpp} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="ogrn" className="col-sm-2 col-form-label">ОГРН</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="ogrn" value={form.ogrn} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="paymentAccount" className="col-sm-2 col-form-label">Расчетный счет</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="paymentAccount" value={form.paymentAccount} onChange={onChangeText}/></div>
+                    </div>
+
+                    <hr/>
+                    <h4>Адрес</h4>
+                    <div className="mb-3 row">
+                        <label htmlFor="postCode" className="col-sm-2 col-form-label">Почтовый индекс</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="postCode" value={form.postCode} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="country" className="col-sm-2 col-form-label">Страна</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="country" value={form.country} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="region" className="col-sm-2 col-form-label">Регион</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="region" value={form.region} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="district" className="col-sm-2 col-form-label">Район</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="district" value={form.district} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="locality" className="col-sm-2 col-form-label">Населенный пункт</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="locality" value={form.locality} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="street" className="col-sm-2 col-form-label">Населенный пункт</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="street" value={form.street} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="house" className="col-sm-2 col-form-label">Дом</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="house" value={form.house} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="corps" className="col-sm-2 col-form-label">Корпус</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="corps" value={form.corps} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="structure" className="col-sm-2 col-form-label">Строение</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="structure" value={form.structure} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="flat" className="col-sm-2 col-form-label">Квартира</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="flat" value={form.flat} onChange={onChangeText}/></div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Добавить</button>
+
+                </form>
             </div>
-            <div className="mb-3">
-                <label htmlFor="inputText" className="form-label">Описание</label>
-                <textarea className="form-control" id="inputText" rows="5" onChange={onChangeText} value={form.inputText}></textarea>
-            </div>
+        </div>
 
-            <br/>
-            <br/>
-
-            <button type="submit" className="btn btn-primary">Добавить</button>
-
-        </form>
     }
 
     return (
