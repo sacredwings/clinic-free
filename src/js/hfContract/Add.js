bfdbb2e6bd7f78df4c8b6fae66a6efb13182cbd0
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2021-10-03 13:41:26 +0700

    Добавление контракта

--- src/js/hfContract/Add.js
+++ src/js/hfContract/Add.js
@@ -4,13 +4,13 @@ import axios from "axios";
 
 function Add (props) {
     const formDefault = {
-        code: '',
         name: '',
         date_from: '',
         date_to: ''
     }
 
     let [form, setForm] = useState(formDefault)
+    let [listTypeSelectValue, setListTypeSelectValue] = useState('')
 
     useEffect(async () => {
     }, [])
@@ -35,11 +35,25 @@ function Add (props) {
 
         let fields = form
         fields.org_id = props.match.params.id
+        fields.price = listTypeSelectValue
 
-        let result = await axios.post(url, form);
+        let result = await axios.post(url, fields);
 
         result = result.data;
 
+    }
+
+    const onChangeType = async (e) => {
+        let OrgId = e.target.value //id выбранной организации из массива
+        setListTypeSelectValue(OrgId) //сохраням в статус
+    }
+
+    const ListType = () => {
+        return <select value={listTypeSelectValue} onChange={onChangeType} className="form-select" aria-label="Default select example">
+            <option value="0">Общий</option>
+            <option value="1">Индивидуальный</option>
+            <option value="2">Фиксируемый</option>
+        </select>
     }
 
     const Form = () => {
@@ -48,10 +62,11 @@ function Add (props) {
 
                 <div className="card-header">Новый договор</div>
                 <div className="card-body">
-                    <div className="mb-3 row">
+
+                    {/* <div className="mb-3 row">
                         <label htmlFor="code" className="col-sm-2 col-form-label">Код</label>
                         <div className="col-sm-10"><input type="text" className="form-control" id="code" value={form.code} onChange={onChangeText}/></div>
-                    </div>
+                    </div> */}
 
                     <div className="mb-3 row">
                         <label htmlFor="name" className="col-sm-2 col-form-label">Наименование</label>
@@ -60,12 +75,24 @@ function Add (props) {
 
                     <div className="mb-3 row">
                         <label htmlFor="date_from" className="col-sm-2 col-form-label">C</label>
-                        <div className="col-sm-10"><input type="text" className="form-control" id="date_from" value={form.date_from} onChange={onChangeText}/></div>
+                        <div className="col-sm-10"><input type="date" className="form-control" id="date_from" value={form.date_from} onChange={onChangeText}/></div>
                     </div>
 
                     <div className="mb-3 row">
                         <label htmlFor="date_to" className="col-sm-2 col-form-label">До</label>
-                        <div className="col-sm-10"><input type="text" className="form-control" id="date_to" value={form.date_to} onChange={onChangeText}/></div>
+                        <div className="col-sm-10"><input type="date" className="form-control" id="date_to" value={form.date_to} onChange={onChangeText}/></div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="type" className="col-sm-2 col-form-label">Тип расчета</label>
+                        <div className="col-sm-10">
+                            {ListType()}
+                        </div>
+                    </div>
+
+                    <div className="mb-3 row">
+                        <label htmlFor="price" className="col-sm-2 col-form-label">Цена за человека</label>
+                        <div className="col-sm-10"><input type="text" className="form-control" id="price" value={form.price} onChange={onChangeText}/></div>
                     </div>
 
                     <button type="submit" className="btn btn-primary">Добавить</button>
