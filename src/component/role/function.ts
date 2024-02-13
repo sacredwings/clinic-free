89a323e5e2d898018d1c2f7198833f3a079ddbd6
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2024-02-13 08:24:30 +0700

    Роли

--- src/component/role/function.ts
+++ src/component/role/function.ts
@@ -1,23 +1,52 @@
 export const accessList = [
-    {name: 'РольЧтение', access: 'roleGet'},
-    {name: 'РольЗапись', access: 'roleEdit'},
-    {name: 'РольСоздание', access: 'roleAdd'},
-    {name: 'РольУдаление', access: 'roleDelete'},
-    {name: 'ПользовательЧтение', access: 'userGet'},
-    {name: 'ПользовательЗапись', access: 'userEdit'},
-    {name: 'ПользовательСоздание', access: 'userAdd'},
-    {name: 'ПользовательУдаление', access: 'userDelete'}
-    /*
-    {category: 'Роль', list: [
-            {name: 'Чтение', access: 'roleGet'},
-            {name: 'Запись', access: 'roleEdit'},
-            {name: 'Создание', access: 'roleAdd'},
-            {name: 'Удаление', access: 'roleDelete'}
-        ]},
-    {category: 'Пользователь', list: [
-            {name: 'Чтение', access: 'userGet'},
-            {name: 'Запись', access: 'userEdit'},
-            {name: 'Создание', access: 'userAdd'},
-            {name: 'Удаление', access: 'userDelete'}
-        ]}*/
+
+    {name: 'Договор - Чтение', access: 'contractGet'},
+    {name: 'Договор - Запись', access: 'contractEdit'},
+    {name: 'Договор - Создание', access: 'contractAdd'},
+    {name: 'Договор - Удаление', access: 'contractDelete'},
+
+    {name: 'Типы договоров - Чтение', access: 'contractTypeGet'},
+    {name: 'Типы договоров - Запись', access: 'contractTypeEdit'},
+    {name: 'Типы договоров - Создание', access: 'contractTypeAdd'},
+    {name: 'Типы договоров - Удаление', access: 'contractTypeDelete'},
+
+    {name: 'Вредный фактор - Чтение', access: 'hfGet'},
+    {name: 'Вредный фактор - Запись', access: 'hfEdit'},
+    {name: 'Вредный фактор - Создание', access: 'hfAdd'},
+    {name: 'Вредный фактор - Удаление', access: 'hfDelete'},
+
+    {name: 'Организация - Чтение', access: 'orgGet'},
+    {name: 'Организация - Запись', access: 'orgEdit'},
+    {name: 'Организация - Создание', access: 'orgAdd'},
+    {name: 'Организация - Удаление', access: 'orgDelete'},
+
+    {name: 'Цена - Чтение', access: 'priceGet'},
+    {name: 'Цена - Запись', access: 'priceEdit'},
+    {name: 'Цена - Создание', access: 'priceAdd'},
+    {name: 'Цена - Удаление', access: 'priceDelete'},
+
+    {name: 'Исследование - Чтение', access: 'researchGet'},
+    {name: 'Исследование - Запись', access: 'researchEdit'},
+    {name: 'Исследование - Создание', access: 'researchAdd'},
+    {name: 'Исследование - Удаление', access: 'researchDelete'},
+
+    {name: 'Роль - Чтение', access: 'roleGet'},
+    {name: 'Роль - Запись', access: 'roleEdit'},
+    {name: 'Роль - Создание', access: 'roleAdd'},
+    {name: 'Роль - Удаление', access: 'roleDelete'},
+
+    {name: 'Специалист - Чтение', access: 'specialistGet'},
+    {name: 'Специалист - Запись', access: 'specialistEdit'},
+    {name: 'Специалист - Создание', access: 'specialistAdd'},
+    {name: 'Специалист - Удаление', access: 'specialistDelete'},
+
+    {name: 'Пользователь - Чтение', access: 'userGet'},
+    {name: 'Пользователь - Запись', access: 'userEdit'},
+    {name: 'Пользователь - Создание', access: 'userAdd'},
+    {name: 'Пользователь - Удаление', access: 'userDelete'},
+
+    {name: 'Работник - Чтение', access: 'workerGet'},
+    {name: 'Работник - Запись', access: 'workerEdit'},
+    {name: 'Работник - Создание', access: 'workerAdd'},
+    {name: 'Работник - Удаление', access: 'workerDelete'},
 ]
