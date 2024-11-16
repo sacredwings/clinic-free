1df0e0979a2550619d0ba74066d57c4e815fe311
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2024-11-16 18:47:03 +0700

    Компиляция

---
+++ src/component/auth/telegram.tsx
@@ -0,0 +1,45 @@
+// @ts-nocheck
+'use client'
+
+import TelegramLoginButton from 'react-telegram-login';
+import {ServerAuthLogin, ServerAuthTelegram} from "@/component/function/url_api";
+import {AuthSet} from "@/store/reducer/myUser";
+import { useAppSelector, useAppDispatch } from '@/store/hooks'
+import {useRouter} from "next/navigation";
+import config from "../../../config.json";
+
+export default function Telegram (props) {
+    const router = useRouter()
+    const dispatch = useAppDispatch()
+
+    const handleTelegramResponse = async responseTg => {
+        //await ServerAuthTelegram
+
+        //проверка ответа сервера
+        //await ToastSystemAdd(result.data)
+        let response = await ServerAuthTelegram(responseTg)
+
+        //авторизация в сторе
+        if (!response) return
+
+        dispatch(AuthSet({
+            _id: response._id,
+            login: response.login,
+
+            message: response.message,
+            friend: response.friend,
+            notify: response.notify,
+            visitor: response.visitor,
+
+            tokenId: response.tid,
+            tokenKey: response.tkey,
+            remember: true
+        }))
+
+        router.push(`/user/${response._id}`)
+    };
+
+    return <>
+        <TelegramLoginButton dataOnauth={handleTelegramResponse} botName={config.telegram.botName} lang={'ru'}/>
+    </>
+}
