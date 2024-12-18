4ad3b8b9a8635c7b4ace2180c86af17db4b497f1
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2024-12-18 11:27:32 +0700

    Добавлен permission

---
+++ src/class/permission.ts
@@ -0,0 +1,79 @@
+/*
+    Права доступа
+*/
+
+// @ts-nocheck
+import { DB, Store } from "../../../social-framework/src"
+
+export default class Permission {
+
+    static async GetById ( ids ) {
+        try {
+            ids = new DB().ObjectID(ids)
+
+            let arAggregate = []
+            arAggregate.push({
+                $match: {
+                    _id: {$in: ids},
+                    delete: {$ne: true}
+                }
+            })
+
+            const mongoClient = Store.GetMongoClient()
+            let collection = mongoClient.collection('permission')
+            let result = await collection.aggregate(arAggregate).toArray()
+            return result
+
+        } catch (err) {
+            console.log(err)
+            throw ({...{err: 7001000, msg: 'CPermission GetById'}, ...err})
+        }
+    }
+
+    static async Get ( fields ) {
+        try {
+            let arAggregate = []
+            arAggregate.push({
+                $match: {
+                    delete: {$ne: true}
+                }
+            })
+
+            const mongoClient = Store.GetMongoClient()
+            let collection = mongoClient.collection('permission')
+            let result = await collection.aggregate(arAggregate).skip(fields.offset).limit(fields.count).toArray()
+            return result
+
+        } catch (err) {
+            console.log(err)
+            throw ({...{err: 7001000, msg: 'CPermission Get'}, ...err})
+        }
+    }
+
+    static async GetCount ( fields ) {
+        try {
+            let arAggregate = []
+            arAggregate.push({
+                $match: {
+                    delete: {$ne: true}
+                }
+            })
+
+            arAggregate.push({
+                $count: 'count'
+            })
+
+            const mongoClient = Store.GetMongoClient()
+            let collection = mongoClient.collection('permission')
+            let result = await collection.aggregate(arAggregate).toArray()
+
+            if (!result.length) return 0
+            return result[0].count
+
+        } catch (err) {
+            console.log(err)
+            throw ({code: 6004000, msg: 'CPermission GetCount'})
+        }
+    }
+
+}
